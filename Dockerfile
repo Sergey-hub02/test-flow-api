daec97e7e395b4911a8bffa7701bb235a9aa4565
Sergey-hub02 <s.park190802@gmail.com>
2026-03-28 15:22:30 +0300

    Создал базу данных для приложения и успешно к ней подключился

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:24.14-alpine
+WORKDIR /app
+COPY package.json ./
+RUN npm install
+COPY . .
+RUN npm run compile
+EXPOSE 3000
+CMD ["npm", "run", "dev"]
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
-import express from "express";
+import express from 'express';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT as unknown as number,
+});
+
+const PORT = process.env.PORT;
 
 const main = async () => {
     const app = express();
+    app.use(express.json());
 
-    app.get('/api/v1/hello/', (_, response) => {
-        response.send('hello world');
+    app.get('/api/v1/hello/',  async (_, response) => {
+        const result = await pool.query('SELECT $1::text AS message', ['Hello World!']);
+        console.log(result.rows[0]);
+        response.send('Подключение к базе данных прошло успешно!');
     });
 
-    app.listen(8080, () => {
+    app.listen(PORT, () => {
         console.log('API запущено на http://localhost:8080/');
     });
 };
